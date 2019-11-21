86576226939d6c2587c9252e9939fe3593c68b40
Carlos <cceiii0706@gmail.com>
2019-11-21 01:12:21 -0700

    third function

--- Week12-utility.py
+++ Week12-utility.py
@@ -9,3 +9,13 @@ def LoadFile(file):
     with open(file, 'r+') as x:
         y = x.readlines()
     return y
+
+def UpdateString(x, y, num):
+    l = []
+    for w in x:
+        l.append(w)
+    l[3] = y
+    out = ''
+    for j in l:
+        out += w
+    print('OUTPUT %s' % out)
